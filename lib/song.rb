1cadebf84a4661e49be9d1ed2bc68c544b411a7b
Travis Lee <travisslee223@gmail.com>
2020-09-18 22:26:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
   
   def self.genre_count
     genre_count = {}
-    @@genres.each do |genre|
+    @@genres.uniq.each do |genre|
       if genre_count[genre]
       genre_count[genre] += 1 
       else
